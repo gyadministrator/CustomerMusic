c1908752a828e22540ea24ec660daf8aa78613e0
gy <1984629668@qq.com>
2019-10-31 23:07:10 +0800

    修改界面。

--- app/src/main/java/com/android/customer/music/MainActivity.java
+++ app/src/main/java/com/android/customer/music/MainActivity.java
@@ -1,27 +1,40 @@
 package com.android.customer.music;
 
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.customer.music.activity.BaseActivity;
+import com.android.customer.music.adapter.LinearAdapter;
 import com.android.customer.music.adapter.RecyclerAdapter;
 import com.android.customer.music.view.RecyclerDecoration;
 
 public class MainActivity extends BaseActivity {
-    private RecyclerView rv;
+    private RecyclerView rv_recommend;
     private RecyclerAdapter recyclerAdapter;
+    private RecyclerView rv_new;
+    private LinearAdapter linerAdapter;
 
     @Override
     protected void initView() {
-        rv = fd(R.id.rv);
+        rv_recommend = fd(R.id.rv_recommend);
+        rv_new = fd(R.id.rv_new);
     }
 
     @Override
     protected void initData() {
-        rv.setLayoutManager(new GridLayoutManager(this, 3));
-        rv.addItemDecoration(new RecyclerDecoration(getResources().getDimensionPixelSize(R.dimen.itemWidth)));
+        rv_recommend.setLayoutManager(new GridLayoutManager(this, 3));
+        rv_recommend.addItemDecoration(new RecyclerDecoration(getResources().getDimensionPixelSize(R.dimen.itemWidth)));
+        rv_recommend.setNestedScrollingEnabled(false);
         recyclerAdapter = new RecyclerAdapter(mActivity);
-        rv.setAdapter(recyclerAdapter);
+        rv_recommend.setAdapter(recyclerAdapter);
+
+        rv_new.setLayoutManager(new LinearLayoutManager(this));
+        rv_new.addItemDecoration(new DividerItemDecoration(this, RecyclerView.VERTICAL));
+        rv_new.setNestedScrollingEnabled(false);
+        linerAdapter = new LinearAdapter(mActivity, rv_new);
+        rv_new.setAdapter(linerAdapter);
     }
 
     @Override
---
+++ app/src/main/java/com/android/customer/music/adapter/LinearAdapter.java
@@ -0,0 +1,64 @@
+package com.android.customer.music.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.customer.music.R;
+
+/**
+ * Description: CustomerMusic
+ * Created by gy(1984629668@qq.com)
+ * Created Time on 2019/10/31 9:13
+ */
+public class LinearAdapter extends RecyclerView.Adapter<LinearAdapter.ViewHolder> {
+    private Context mContext;
+    private View mItemView;
+    private RecyclerView mRecyclerView;
+    private boolean isCalculationRvHeight;
+
+    public LinearAdapter(Context mContext, RecyclerView mRecyclerView) {
+        this.mContext = mContext;
+        this.mRecyclerView = mRecyclerView;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        mItemView = LayoutInflater.from(mContext).inflate(R.layout.linear_adapter, parent, false);
+        return new ViewHolder(mItemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        setRecyclerViewHeight();
+    }
+
+    private void setRecyclerViewHeight() {
+        if (isCalculationRvHeight || mRecyclerView == null) return;
+        isCalculationRvHeight = true;
+        RecyclerView.LayoutParams itemViewLp = (RecyclerView.LayoutParams) mItemView.getLayoutParams();
+        int itemCount = getItemCount();
+        int recyclerViewHeight = itemViewLp.height * itemCount;
+        LinearLayout.LayoutParams rvLp = (LinearLayout.LayoutParams) mRecyclerView.getLayoutParams();
+        rvLp.height = recyclerViewHeight;
+        mRecyclerView.setLayoutParams(rvLp);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 6;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
